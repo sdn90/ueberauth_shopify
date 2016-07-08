522429419d634554456eb3580a7f7cdd1d2e9061
Steven Nguyen <sdnguyen90@gmail.com>
2016-07-08 14:24:35 -0700

    fix dependencies

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule UeberauthShopify.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :oauth2]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule UeberauthShopify.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:oauth2, "~> 0.6"}]
   end
 end
